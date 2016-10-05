a330f621fcafa924ceb976a209f0aee1259eb68d
Valverde Antonio <tokageroh@gmail.com>
2016-10-05 16:32:10 +0900

    Rename first letter of constructor method filename to capital letter.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
 // expose methods
-exports.QueryBuilder = require('./queryBuilder');
+exports.QueryBuilder = require('./QueryBuilder');
 exports.leafQueries = require('./leafQueries');
 exports.compoundQueries = require('./compoundQueries');
